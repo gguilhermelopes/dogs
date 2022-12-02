7e8ceb10d52fd2527ab0e70c368618505c655e65
gguilhermelopes <guilhermelopes275@gmail.com>
2022-12-02 04:14:29 -0300

    login e cadastro de usuários

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,7 +5,7 @@ import { ReactComponent as Dogs } from "../Assets/dogs.svg";
 import { UserContext } from "../userContext";
 
 const Header = () => {
-  const { data } = React.useContext(UserContext);
+  const { data, userLogout } = React.useContext(UserContext);
 
   return (
     <header className={styles.header}>
@@ -22,6 +22,7 @@ const Header = () => {
             Login / Criar
           </Link>
         )}
+        {data && <button onClick={userLogout}>LOGOUT</button>}
       </nav>
     </header>
   );
--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -6,6 +6,11 @@ const types = {
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: "Preencha um email válido.",
   },
+  password: {
+    regex: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d\w\W]{8,}$/,
+    message:
+      "A senha deve conter no mínimo 8 caracteres, com uma letra maiúscula, uma letra minúscula e um número.",
+  },
 };
 
 const useForm = (type) => {
