87b23a73c03e3fdbf199f0fdecf0359c6631a00f
gguilhermelopes <guilhermelopes275@gmail.com>
2022-12-04 14:47:29 -0300

    modal outside click

--- src/Components/Feed/FeedPhoto.js
+++ src/Components/Feed/FeedPhoto.js
@@ -12,8 +12,7 @@ const FeedPhoto = ({ setModalPhoto }) => {
   React.useEffect(() => {
     async function fetchPhotos() {
       const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
-      const { response, json } = await request(url, options);
-      console.log(response, json);
+      await request(url, options);
     }
     fetchPhotos();
   }, [request]);
--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import PhotoComments from "./PhotoComments";
 import styles from "./PhotoContent.module.css";
 
 const PhotoContent = ({ data }) => {
@@ -9,6 +11,26 @@ const PhotoContent = ({ data }) => {
       <div className={styles.img}>
         <img src={photo.src} alt={photo.title} />
       </div>
+      <div className={styles.details}>
+        <div>
+          <p className={styles.author}>
+            <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            <span className={styles.visualizacoes}>{photo.acessos}</span>
+          </p>
+          <h1 className="title">
+            <Link to={`/photo/${photo.id}`}>{photo.title}</Link>
+          </h1>
+          <ul className={styles.attributes}>
+            <li>{photo.peso} Kg</li>
+            {photo.idade === "1" ? (
+              <li>{photo.idade} ano</li>
+            ) : (
+              <li>{photo.idade} anos</li>
+            )}
+          </ul>
+        </div>
+      </div>
+      <PhotoComments id={photo.id} comments={comments} />
     </div>
   );
 };
